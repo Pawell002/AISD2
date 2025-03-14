1eff23747da626cb549d1f6f2f91319777fdb37d
KonradZd <conrad.zdziarski@gmail.com>
2025-03-14 21:39:43 +0100

    Update zadanie4.cpp

--- ZADANIE 2: Przepływ w sieci/zadanie4.cpp
+++ ZADANIE 2: Przepływ w sieci/zadanie4.cpp
@@ -1,22 +1,63 @@
 #include <iostream>
 #include <vector>
+#include <limits.h>
 
 using namespace std;
 
-void odwiedzone(vector<vector<int>> &lista, vector<bool> &wierzcholki, int start){
-    wierzcholki[start] = true;
 
-    for(int i : lista[start]){
-        if(wierzcholki[i] == false){
-            odwiedzone(lista, wierzcholki, i);
-        }
+bool DFS(vector<vector<int>> &macierz, vector<bool> &wierzcholki, vector<int> &sciezka, int start, int koniec){
+    wierzcholki[start] = true;
+    if(start == koniec){
+        return true;
     }
 
+    for(int i = 0; i < macierz.size(); i++){
+        if(!wierzcholki[i] && macierz[start][i] > 0){
+            sciezka[i] = start;
+            if(DFS(macierz, wierzcholki, sciezka, i, koniec)){
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
-void DFS(vector<vector<int>> lista, int start){
-    vector<bool> wierzcholki(lista.size(), false);
-    odwiedzone(lista, wierzcholki, start);
+bool sprawdzPolaczenie(vector<vector<int>> &macierz, vector<int> &sciezka, int start, int koniec){
+    vector<bool> wierzcholki(macierz.size(), false);
+    return DFS(macierz, wierzcholki, sciezka, start, koniec);
+}
+
+int FordFulkerson(vector<vector<int>> &macierz, int start, int koniec){
+    int n = macierz.size();
+    vector<vector<int>> mac_residualna = macierz;
+    vector<int> sciezka(n, -1);
+    int maxPrzeplyw = 0;
+    int przeplyw, v, u;
+
+    while(sprawdzPolaczenie(mac_residualna, sciezka, start, koniec)){
+        przeplyw = INT_MAX; //symbol nieskonczonosci
+
+        v = koniec;
+
+        while(v != start){
+            u = sciezka[v];
+            przeplyw = min(przeplyw, mac_residualna[u][v]);
+            v = u;
+        }
+
+        v = koniec;
+
+        while(v != start){
+            u = sciezka[v];
+            mac_residualna[u][v] -= przeplyw;
+            mac_residualna[v][u] += przeplyw;
+            v = u;
+        }
+
+        maxPrzeplyw += przeplyw;
+    }
+
+    return maxPrzeplyw;
 }
 
 int main(){
@@ -26,19 +67,15 @@ int main(){
     cin >> n >> m;
 
     vector<vector<int>> macierz(n, vector<int>(n, 0));
-    vector<vector<int>> lista(n);
 
     for(int i = 0; i < m; i++){
         cin >> u >> v >> wart;
         macierz[u][v] = wart;
-        lista[u].push_back(v);
     }
 
-    while(true){
+    wynik = FordFulkerson(macierz, 0, n-1);
 
-        break;
-    }
-    DFS(lista, 0);
+    cout << wynik << endl;
 
     return 0;
 }
