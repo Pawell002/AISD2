a55a1b22af26f7b03430620515bebb59023a7acc
Pawell002 <pawelxx-70@wp.pl>
2025-05-21 12:41:36 +0200

    Create reprezentacja_danych.h

---
+++ PROJEKT/Kody/min_max_flow_i_tekstowe/reprezentacja_danych.h
@@ -0,0 +1,43 @@
+#ifndef REPREZENTACJA_DANYCH_H
+#define REPREZENTACJA_DANYCH_H
+#include <iostream>
+#include <vector>
+#include <map>
+
+struct WynikTrie {
+    int linia;
+    int pozycja;
+    int wzorzecID;
+};
+
+struct Pole {
+    int id;
+    double x;
+    double y;
+    int ilosc_jeczmienia;
+};
+
+struct Browar {
+    int id;
+    double x;
+    double y;
+    int pojemnosc;
+};
+
+struct Karczma {
+    int id;
+    double x;
+    double y;
+    int zapotrzebowanie;
+};
+
+struct Droga {
+    int id;
+    int zrodlo_id;
+    int cel_id;
+    std::string produkt;
+    int przepustowosc;
+    int koszt_naprawy;
+};
+
+#endif
