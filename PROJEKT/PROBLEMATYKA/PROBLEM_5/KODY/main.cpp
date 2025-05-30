a24ccce47c4323d4e91ca11edf625908a3da1105
Pawell002 <pawelxx-70@wp.pl>
2025-05-30 18:57:51 +0200

    Update main.cpp

--- PROJEKT/PROBLEMATYKA/PROBLEM_5/KODY/main.cpp
+++ PROJEKT/PROBLEMATYKA/PROBLEM_5/KODY/main.cpp
@@ -220,5 +220,20 @@ cout << "\nLaczna ilosc jeczmienia: " << laczna_ilosc_jeczmienia << endl;
         cin >> wybor;
     }
 
+    Huffman huffman;
+    std::string tekst;
+
+    std::cout << "Podaj tekst do kompresji: ";
+    std::getline(std::cin, tekst);
+
+    huffman.kompresuj(tekst);
+
+    std::cout << "\nZakodowany tekst (binarnie):\n" << huffman.getZakodowanyTekst() << "\n";
+    huffman.wypiszKody();
+
+    std::string zdekompresowany = huffman.dekompresuj(huffman.getZakodowanyTekst());
+
+    std::cout << "\nZdekompresowany tekst:\n" << zdekompresowany << "\n";
+
     return 0;
 }
