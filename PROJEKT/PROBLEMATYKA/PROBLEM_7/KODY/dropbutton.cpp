2bebe7e605016756ebf1a52fc9cdfd9e9f25df06
kamillll1 <kalenalewajski@onet.pl>
2025-06-03 00:09:03 +0200

    Update dropbutton.cpp

--- PROJEKT/PROBLEMATYKA/PROBLEM_7/KODY/dropbutton.cpp
+++ PROJEKT/PROBLEMATYKA/PROBLEM_7/KODY/dropbutton.cpp
@@ -1,1 +1,22 @@
+#include "dropbutton.h"
+#include <QMimeData>
+#include <QUrl>
+
+DropButton::DropButton(QWidget* parent)
+    : QPushButton(parent){
+    setAcceptDrops(true);
+}
+
+void DropButton::dragEnterEvent(QDragEnterEvent* zdarzenie){
+    if (zdarzenie->mimeData()->hasUrls()) {
+        zdarzenie->acceptProposedAction();
+    }
+}
+
+void DropButton::dropEvent(QDropEvent* zdarzenie){
+    if (zdarzenie->mimeData()->hasUrls()) {
+        QUrl url = zdarzenie->mimeData()->urls().first();
+        emit plikUpuszczony(url.toLocalFile());
+    }
+}
 
