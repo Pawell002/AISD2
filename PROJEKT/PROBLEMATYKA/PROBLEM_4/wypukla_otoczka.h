9f181285efd51faa77e48ca9a42c9320abd3f277
Pawell002 <pawelxx-70@wp.pl>
2025-06-03 14:41:54 +0200

    Update wypukla_otoczka.h

--- PROJEKT/PROBLEMATYKA/PROBLEM_4/wypukla_otoczka.h
+++ PROJEKT/PROBLEMATYKA/PROBLEM_4/wypukla_otoczka.h
@@ -5,11 +5,13 @@
 
 struct Punkt {
     double x, y;
-    Punkt();
-    Punkt(double _x, double _y);
+    Punkt() : x(0), y(0) {}
+    Punkt(double _x, double _y) : x(_x), y(_y) {}
 };
 
+// Funkcje zadeklarowane – definicje są w wypukla_otoczka.cpp
 std::vector<Punkt> wypuklaOtoczka(std::vector<Punkt> &punkty);
 double poleWielokata(const std::vector<Punkt> &wielokat);
 
-#endif // WYPUKLA_OTOCKA_H
+#endif // WYPUKLA_OTOCZKA_H
+
